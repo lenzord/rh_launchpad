870312e4b81ff17aa89a7f34927bb0c6af9525da
lenz <lenzordmusic@gmail.com>
2022-08-12 12:07:28 -0400

    front almost done

---
+++ src/App.tsx
@@ -0,0 +1,401 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="main-header">
+        <div className="header-logo">
+          <a href="logo-link">
+            <img alt="company-logo" />
+          </a>
+        </div>
+        <div className="header-content">
+          <a href="#">HOME</a>
+          <a href="marketplace">PROJECT</a>
+          <a href="staking">STAKING</a>
+        </div>
+        <div className="buy-btn">
+          <a href="buy-token">BUY TOKEN</a>
+        </div>
+      </header >
+      <section className="hero-card">
+        <div className="hero-left">
+          <div className="main-hero-text">
+            <h2>
+              METAVERSE
+              PLAY-TO  EARN IGO LAUNCHPAD
+            </h2>
+          </div>
+          <div className="secondary-hero-text">
+            <h4>
+              The next generation gaming ecosystem for IGOs and NFT market Secure crypto solutions by blockchain technology
+            </h4>
+          </div>
+          <div className="hero-btns">
+            <a href="#">
+              VIEW PROJECTS
+            </a>
+            <a href="#">
+              APPLY PROJECT
+            </a>
+          </div>
+        </div>
+        <div className="hero-right">
+          <div className="hero-img">
+            <img alt="finance-pik" />
+          </div>
+          <div className="div-hero-list">
+            <ul className="hero-list">
+              <li>
+                <h5>MARKET CAP</h5>
+                <h6>$420.69M</h6>
+              </li>
+              <li>
+                <h5>TVL</h5>
+                <h6>$12.94M</h6>
+              </li>
+              <li>
+                <h5>FUND RAISED</h5>
+                <h6>$149.86M</h6>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="project-pools">
+        <div className="pool-inner-sec">
+          <div className="pool-inner-header">
+            <div className="sub-inner">
+              <span className="subtitle">EXPLORE</span>
+              <img className="sub-left-image-steps" alt="Steps-image" />
+            </div>
+            <h2>PROJECT POOLS</h2>
+          </div>
+          <div className="pool-btn-area">
+            <a href="on-going-pools">ON GOING</a>
+            <a href="upcoming-pools">UPCOMING</a>
+          </div>
+        </div>
+        <div>
+          <div className="pool-coin-sec">
+            <ul className="menu-list">
+              <li className="list-1">PROJECT NAME</li>
+              <li className="list-2">CHAIN</li>
+              <li className="list-3">LAUNCHED</li>
+              <li className="list-4">TOTAL RAISE</li>
+              <li className="list-5">PROGRESS</li>
+            </ul>
+          </div>
+          <a href="project-details" id="pool-card-btn">
+            <div id="pool-coin-card">
+              <img alt="coin-img" className="card-coin-img" />
+              <span>
+                <h3>KyberDyne</h3>
+                <h5>PRICE (GAC) = 0.59 BUSD</h5>
+              </span>
+              <img alt="chain-img" className="card-chain" />
+              <h3>5 Hours ago</h3>
+              <h3>100,000 BUSD (100%)</h3>
+              <h3>progress bar</h3>
+            </div>
+          </a>
+          <a href="project-details" id="pool-card-btn">
+            <div id="pool-coin-card">
+              <img alt="coin-img" className="card-coin-img" />
+              <span>
+                <h3>Thetan Arena</h3>
+                <h5>PRICE (FSC) = 0.89 BUSD</h5>
+              </span>
+              <img alt="chain-img" className="card-chain" />
+              <h3>1 day ago</h3>
+              <h3>256,000 BUSD (73%)</h3>
+              <h3>progress bar</h3>
+            </div>
+          </a>
+          <a href="project-details" id="pool-card-btn">
+            <div id="pool-coin-card">
+              <img alt="coin-img" className="card-coin-img" />
+              <span>
+                <h3>Galaxy War</h3>
+                <h5>PRICE (MTS) = 0.33 BUSD</h5>
+              </span>
+              <img alt="chain-img" className="card-chain" />
+              <h3>4 days ago</h3>
+              <h3>510,000 BUSD (98%)</h3>
+              <h3>progress bar</h3>
+            </div>
+          </a>
+          <a href="project-details" id="pool-card-btn">
+            <div id="pool-coin-card">
+              <img alt="coin-img" className="card-coin-img" />
+              <span>
+                <h3>KingdomX</h3>
+                <h5>PRICE (CTC) = 0.45 BUSD</h5>
+              </span>
+              <img alt="chain-img" className="card-chain" />
+              <h3>5 days ago</h3>
+              <h3>100,000 BUSD (100%)</h3>
+              <h3>progress bar</h3>
+            </div>
+          </a>
+          <a href="project-details" id="pool-card-btn">
+            <div id="pool-coin-card">
+              <img alt="coin-img" className="card-coin-img" />
+              <span>
+                <h3>Cyber City</h3>
+                <h5>PRICE (GAC) = 0.59 BUSD</h5>
+              </span>
+              <img alt="chain-img" className="card-chain" />
+              <h3>5 days ago</h3>
+              <h3>500,000 BUSD (86%)</h3>
+              <h3>progress bar</h3>
+            </div>
+          </a>
+        </div>
+      </section>
+      <section className="participate-info">
+        <div className="participate-container">
+          <div className="participate-left-sec">
+            <div className="header">
+              <span className="subtitle">3 EASY STEPS</span>
+              <img className="sub-left-image-steps" alt="Steps-image" />
+            </div>
+            <h1>HOW TO PARTICIPATE</h1>
+            <p>we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment. so blinded by desire, that they cannot foresee the pain and trouble</p>
+            <div className="verify-btn-container">
+              <a href="verift-progress" className="btn-text">
+                <span>VERIFY KYC</span>
+              </a>
+            </div>
+          </div>
+          <div className="participate-right-sec">
+            <div className="participate-right-card">
+              <div className="participate-card-number-one">
+                <img alt="one" />
+              </div>
+              <div className="participate-card-text">
+                <h4>Submit KYC</h4>
+                <p>
+                  Contrary to popular belief, that are bound to ensue; and equal blame belongs to those who fail in their dutyis.
+                </p>
+              </div>
+            </div>
+            <div className="participate-right-card">
+              <div className="participate-card-number-two">
+                <img alt="two" />
+              </div>
+              <div className="participate-card-text">
+                <h4>Verify Wallet</h4>
+                <p>
+                  Simply random text. It has roots in a piece Contrary to popular belief, Lorem Ipsum is not simply random text.
+                </p>
+              </div>
+            </div>
+            <div className="participate-right-card">
+              <div className="participate-card-number-three">
+                <img alt="three" />
+              </div>
+              <div className="participate-card-text">
+                <h4>Start Staking</h4>
+                <p>
+                  It has roots in a piece Contrary to popular belief, Lorem Ipsum is not simply random text roots in a piece
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="key-points">
+        <div className="key-points-header">
+          <div className="sub-inner">
+            <span className="subtitle">FEATURES</span>
+            <img className="sub-left-image-steps" alt="Steps-image" />
+          </div>
+          <h1>KEY-POINTS</h1>
+        </div>
+        <div className="key-points-content">
+          <div className="key-points-card">
+            <div className="card-img-1">
+              <img alt="key-point-icon" />
+            </div>
+            <div className="key-point-text">
+              <h3>Decentralized</h3>
+              <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing</p>
+            </div>
+          </div>
+          <div className="key-points-card">
+            <div className="card-img-2">
+              <img alt="key-point-icon" />
+            </div>
+            <div className="key-point-text">
+              <h3>Secure Bridge</h3>
+              <p>But I must explain to you how all this mistaken idea of new gem denouncing pleasure and praising pain</p>
+            </div>
+          </div>
+          <div className="key-points-card">
+            <div className="card-img-3">
+              <img alt="key-point-icon" />
+            </div>
+            <div className="key-point-text">
+              <h3>Fair Allocations</h3>
+              <p>When our power of choice is untrammelled and when nothing prevents our being able to do what we like best</p>
+            </div>
+          </div>
+          <div className="key-points-card">
+            <div className="card-img-4">
+              <img alt="key-point-icon" />
+            </div>
+            <div className="key-point-text">
+              <h3>Autopilot Liquidity</h3>
+              <p>But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="partners-sec">
+        <div className="partners-header">
+          <div className="partners-inner-header">
+            <div className="sub-inner">
+              <span className="subtitle">BACKERS</span>
+              <img className="sub-left-image-steps" alt="Steps-image" />
+            </div>
+            <h2>OUR PARTNERS</h2>
+          </div>
+          <div className="partners-btn-area">
+            <a href="investor">INVESTOR</a>
+            <a href="media">MEDIA</a>
+          </div>
+        </div>
+        <div className="partners-content">
+          <div className="partner-1">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-2">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-3">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-4">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-5">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-6">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-7">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-8">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-9">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-10">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-11">
+            <img alt="partner-logo"></img>
+          </div>
+          <div className="partner-12">
+            <img alt="partner-logo"></img>
+          </div>
+        </div>
+      </section>
+      <section className="faq-sec">
+        <div className="faq-header">
+          <div className="faq-inner-header">
+            <div className="faq-inner">
+              <img className="sub-left-image-steps2" alt="Steps-image2" />
+              <span className="subtitle-faq">QUESTIONS & ANSWERS</span>
+              <img className="sub-left-image-steps" alt="Steps-image" />
+            </div>
+            <h2>FREQUENTLY ASKED QUESTIONS</h2>
+          </div>
+        </div>
+        <div className="container">
+          <div className="faq-accordion">
+            <div className="accordion-item" id="question-1">
+              <a className="accordion-link" href="#question-1">
+                first question?
+                <span className="material-symbols-outlined">
+                  add
+                </span>
+                <span className="material-symbols-outlined">
+                  remove
+                </span>
+              </a>
+              <div className="answer">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+            </div>
+            <div className="accordion-item" id="question-2">
+              <a className="accordion-link" href="#question-2">
+                second question?
+                <span className="material-symbols-outlined">
+                  add
+                </span>
+                <span className="material-symbols-outlined">
+                  remove
+                </span>
+              </a>
+              <div className="answer">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+            </div>
+            <div className="accordion-item" id="question-3">
+              <a className="accordion-link" href="#question-3">
+                third question?
+                <span className="material-symbols-outlined">
+                  add
+                </span>
+                <span className="material-symbols-outlined">
+                  remove
+                </span>
+              </a>
+              <div className="answer">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+            </div>
+            <div className="accordion-item" id="question-4">
+              <a className="accordion-link" href="#question-4">
+                fourth question?
+                <span className="material-symbols-outlined">
+                  add
+                </span>
+                <span className="material-symbols-outlined">
+                  remove
+                </span>
+              </a>
+              <div className="answer">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+            </div>
+            <div className="accordion-item" id="question-5">
+              <a className="accordion-link" href="#question-5">
+                fifth question?
+                <span className="material-symbols-outlined">
+                  add
+                </span>
+                <span className="material-symbols-outlined">
+                  remove
+                </span>
+              </a>
+              <div className="answer">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="ft">
+        this is a footer
+      </footer>
+    </div >
+  )
+}
+
+export default App
